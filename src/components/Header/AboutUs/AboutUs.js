3f2de8abd2e170150436b38aec60626b0df1c155
NgoHaTriBao <ngobao3553@gmail.com>
2021-10-31 01:51:46 +0700

    Bao up about us

--- src/components/Header/AboutUs/AboutUs.js
+++ src/components/Header/AboutUs/AboutUs.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Footer from "../../Footer";
 import Header from '../../Header';
+import "../../css/about_us.css";
 
 
 export default function AboutUs() {
@@ -9,7 +10,43 @@ export default function AboutUs() {
         <div className="mt-20">
             <Header >
                
-               Aboutus
+            
+          <header id="masthead">
+            
+              <p id="masthead-intro">Hi, welcom to </p> 
+              <h1 id="masthead-heading">Appointment mentor scheduling applications</h1>
+          </header>
+        
+    
+      
+      <section id="introduction">
+        <div id="starcontainer">
+          <h2>About us</h2>
+          <i id="fa fa-star fa-2x"></i>
+          <hr id="star"></hr>
+        </div>
+          <p>Our application development team is from FPT University. </p>
+      
+          <p>In the process of studying and experiencing at the school, we realize that many students have difficulties in studying and orienting their future jobs. Mentor is someone who can answer students' problems but it is difficult to meet a mentor outside of id time. So we created this website with the purpose of creating a connection between students and mentors quickly, easily and saving time. </p>
+      </section>
+      
+      <section id="location">
+          <h1>Where I’m From</h1>
+          <p>....</p>
+      </section>
+      
+      
+      
+      
+      
+      <footer id="content-footer">
+        <p>Say hi to me on these social networks:</p>
+          <ul id="social">
+
+            
+          </ul>
+       
+      </footer>
                
 
                 <Footer />
