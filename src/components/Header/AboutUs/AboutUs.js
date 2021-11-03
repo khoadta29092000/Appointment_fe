b30fd3c0564af15358e238384a786cb1fa9b39c5
DuyDan2706 <duydan272000@gmail.com>
2021-11-03 21:26:09 +0700

    update lan 2

--- src/components/Header/AboutUs/AboutUs.js
+++ src/components/Header/AboutUs/AboutUs.js
@@ -22,6 +22,7 @@ export default function AboutUs() {
       <section id="introduction">
         <div id="starcontainer">
           <h2>About us</h2>
+        
           <i id="fa fa-star fa-2x"></i>
           <hr id="star"></hr>
         </div>
@@ -34,8 +35,23 @@ export default function AboutUs() {
           <h1>Where I’m From</h1>
           <p>....</p>
       </section>
-      
-      
+      <div id="services">
+   <div id="service_content">
+     <div id="services__items">
+      <img src="https://png.pngtree.com/png-vector/20190711/ourlarge/pngtree-house-icon-real-estate-graphic-design-template-vector-png-image_1542067.jpg" className="img" alt=""/>
+      <h2>Dịch vụ tư vấn</h2>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis temporibus nesciunt asperiores a, maxime excepturi, incidunt, dolores nam harum voluptate dignissimos pariatur dolorum. Repellat odio maxime nostrum corrupti natus beatae.</p>
+     </div>
+    
+     <div id="services__items">
+      {/* <i class="fa fa-user-alt"></i> */}
+      <img src="https://www.pngitem.com/pimgs/m/256-2560208_person-icon-black-png-transparent-png.png" className="img" alt=""/>
+      <h2>Đội ngũ nhân lực</h2>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis temporibus nesciunt asperiores a, maxime excepturi, incidunt, dolores nam harum voluptate dignissimos pariatur dolorum. Repellat odio maxime nostrum corrupti natus beatae.</p>
+     </div>
+   </div>
+  
+ </div>
       
       
 
